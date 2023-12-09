ef3b73586cfa49d8571e2c4cd1fb49d08e4c4ebd
Dipak Bundheliya <bundheliyadeep@gmail.com>
2023-12-09 22:11:26 +0530

    commit

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+const main = async () => {
+  await mongoose.connect(
+    "mongodb+srv://bundheliyadeep:Deep1234@cluster0.hpg57sk.mongodb.net/ecommerce?retryWrites=true&w=majority"
+  );
+  console.log("database connected");
+};
+main().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("hey body");
